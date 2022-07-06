a184006ba4064616ac60a6468b31001e7ed69ab0
vladislav.rep <vladislav.rep@umbrellio.biz>
2022-07-06 11:09:17 +0300

    more convinient partial

--- app/helpers/operations_helper.rb
+++ app/helpers/operations_helper.rb
@@ -8,4 +8,14 @@ module OperationsHelper
     def human_amount(amount, currency)
         "#{amount} #{CURRENCIES.fetch(currency.downcase.to_sym, currency)}"
     end
+
+    def button_text
+        if controller.action_name == "new"
+           return "Save"
+        elsif controller.action_name == "edit"
+           return "Edit"
+        else
+           return "Submit"
+        end
+      end
 end
