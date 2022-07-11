e5c62ad19e0a8e729fb7ac127d3967f09070ae4a
vladislav.rep <repin_vladislav_spb@mail.ru>
2022-07-11 19:12:40 +0300

    fix tests

--- test/integration/category_flow_test.rb
+++ test/integration/category_flow_test.rb
@@ -5,17 +5,20 @@ require 'test_helper'
 class CategoryFlowTest < ActionDispatch::IntegrationTest
   test 'can see the welcome page' do
     get '/'
+
     assert_response :success
   end
 
   test 'can see the categories list' do
     get '/categories'
+
     assert_response :success
   end
 
   test 'can create an caregory' do
     post '/categories',
          params: { category: { name: 'test' } }
+
     assert_response :redirect
     follow_redirect!
     assert_response :success
@@ -24,8 +27,10 @@ class CategoryFlowTest < ActionDispatch::IntegrationTest
 
   test 'can edit an caregory' do
     category = Category.create(name: 'test')
+
     put "/categories/#{category.id}",
         params: { category: { name: 'test2' } }
+
     follow_redirect!
     assert_response :success
     assert_select 'span', "Category 'test' successfully updated to 'test2'!"
@@ -33,7 +38,9 @@ class CategoryFlowTest < ActionDispatch::IntegrationTest
 
   test 'can delete an caregory' do
     category = Category.create(name: 'test')
+
     delete "/categories/#{category.id}"
+    
     assert_response :see_other
     follow_redirect!
     assert_select 'span', "Category 'test' successfully deleted!"
--- test/integration/operation_flow_test.rb
+++ test/integration/operation_flow_test.rb
@@ -5,18 +5,22 @@ require 'test_helper'
 class OperationFlowTest < ActionDispatch::IntegrationTest
   test 'can see the operations list' do
     get '/operations'
+
     assert_response :success
   end
 
   test 'can create an operation' do
     category = Category.create(name: 'test')
-    operation = post '/operations',
-                     params: { operation: { category: category, direction: 'income', date: '2020-01-01', amount: 100,
+    
+    post '/operations',
+                     params: { operation: { category_id: category.id, direction: 'income', date: '2020-01-01', amount: 100,
                                             currency: 'RUB' } }
-    p Operation.all # не получается создать операцию
     assert_response :redirect
     follow_redirect!
     assert_response :success
+
+    operation=Operation.find_by(amount: 100)
+
     assert_select 'span', "Operation '#{operation.id}' successfully saved!"
   end
 
@@ -25,9 +29,11 @@ class OperationFlowTest < ActionDispatch::IntegrationTest
     category2 = Category.create(name: 'test2')
     operation = Operation.create(category: category, direction: 'income', date: '2020-01-01', amount: 200,
                                  currency: 'RUB')
+
     put "/operations/#{operation.id}",
         params: { operation: { category: category2, direction: 'expenditure', date: '2022-02-02', amount: 300,
                                currency: 'USD' } }
+
     follow_redirect!
     assert_response :success
     assert_select 'span', 'Operation successfully updated!'
@@ -37,9 +43,11 @@ class OperationFlowTest < ActionDispatch::IntegrationTest
     category = Category.create(name: 'test')
     operation = Operation.create(category: category, direction: 'income', date: '2020-01-01', amount: 200,
                                  currency: 'RUB')
+
     delete "/operations/#{operation.id}"
+
     assert_response :see_other
     follow_redirect!
-    assert_select 'span', "Category '#{operation.id}' successfully deleted!" # почему-то в этом месте теста ошибка((
+    assert_select 'span', "Operation '#{operation.id}' successfully deleted!"
   end
 end
