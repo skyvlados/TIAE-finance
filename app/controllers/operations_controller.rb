a07357db4504c38dd364365b9058379e90f12231
vladislav.rep <repin_vladislav_spb@mail.ru>
2022-07-18 16:30:37 +0300

    add date filters

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -7,6 +7,8 @@ class OperationsController < ApplicationController
     FILTERS.each do |filter|
       scope = scope.where(filter => params[filter]) if params[filter].present?
     end
+    scope = scope.where("date>= ?", params[:date_start]) if params[:date_start].present?
+    scope = scope.where("date<= ?", params[:date_finish]) if params[:date_finish].present?
 
     @pagy, @operations = pagy(scope, items: params[:page_size])
     
