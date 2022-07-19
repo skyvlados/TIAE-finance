d99e2b42145d707afd6d8c3eb10e05f90a0c4491
vladislav.rep <repin_vladislav_spb@mail.ru>
2022-07-19 17:54:45 +0300

    add operation query test

--- app/services/operation_query.rb
+++ app/services/operation_query.rb
@@ -9,12 +9,12 @@ class OperationQuery
   end
 
   def call
-    scope = Operation.order(id: :asc)
+    scope = Operation.order(date: :desc)
     FILTERS.each do |filter|
       scope = scope.where(filter => params[filter]) if params[filter].present?
     end
-    scope = scope.where("date>= ?", params[:date_start]) if params[:date_start].present?
-    scope = scope.where("date<= ?", params[:date_finish]) if params[:date_finish].present?
+    scope = scope.where('date>= ?', params[:date_start]) if params[:date_start].present?
+    scope = scope.where('date<= ?', params[:date_finish]) if params[:date_finish].present?
     scope
   end
 end
---
+++ test/units/operation_query_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+class OperationQueryTest < ActiveSupport::TestCase
+
+    test 'by direction' do 
+        assert_equal OperationQuery.new(direction: 'income').call.ids, [operations(:salary).id]
+    end
+
+    test 'by category' do
+        assert_equal OperationQuery.new(category: Category.find_by(name: 'relax').id).call.to_a, [operations(:relax)]
+    end
+
+    test 'by currency' do     
+        assert_equal OperationQuery.new(currency: 'EUR').call.to_a, [operations(:food)]
+    end
+
+    test 'by date start' do     
+        assert_equal OperationQuery.new(date_start: '2020-01-05').call.to_a, operations(:food, :relax, :each_goods, :goods)
+    end
+
+    test 'by date finish' do     
+        assert_equal OperationQuery.new(date_finish: '2020-01-05').call.to_a, operations(:goods, :salary)
+    end
+
+    test 'by direction, category, currency, date' do     
+        assert_equal OperationQuery.new(direction: 'expenditure', currency: 'RUB', category: Category.find_by(name: 'goods').id, date_start: '2020-01-05', date_finish: '2020-01-05').call.to_a, [operations(:goods)]
+    end
+end
