8c47ff6a3d0274c1605d9ae473711793f64808b9
vladislav.rep <repin_vladislav_spb@mail.ru>
2022-07-26 22:46:45 +0300

    fix controller tests

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,32 +4,45 @@ require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
-    get categories_index_url
+    get categories_path
     assert_response :success
   end
 
   test 'should get show' do
-    get categories_show_url
+    Category.create(name: 'Test name')
+    test_id = Category.find_by(name: 'Test name').id
+    get category_path(test_id)
     assert_response :success
   end
 
   test 'should get new' do
-    get categories_new_url
+    get new_category_path
     assert_response :success
   end
 
   test 'should get create' do
-    get categories_create_url
-    assert_response :success
+    post categories_path, params: { category: { name: 'test' } }
+    assert_response :found
   end
 
   test 'should get edit' do
-    get categories_edit_url
+    Category.create(name: 'Test name')
+    test_id = Category.find_by(name: 'Test name').id
+    get edit_category_path(test_id)
     assert_response :success
   end
 
+  test 'should get update' do
+    Category.create(name: 'Test name')
+    test_id = Category.find_by(name: 'Test name').id
+    patch category_path(test_id), params: { category: { name: 'test' } }
+    assert_response :found
+  end
+
   test 'should get destroy' do
-    get categories_destroy_url
-    assert_response :success
+    Category.create(name: 'Test name')
+    test_id = Category.find_by(name: 'Test name').id
+    delete category_path(test_id)
+    assert_response :see_other
   end
 end
--- test/controllers/operations_controller_test.rb
+++ test/controllers/operations_controller_test.rb
@@ -4,32 +4,61 @@ require 'test_helper'
 
 class OperationsControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
-    get operations_index_url
+    get operations_path
     assert_response :success
   end
 
   test 'should get show' do
-    get operations_show_url
+    Category.create(name: 'Test name')
+    test_category_id = Category.find_by(name: 'Test name').id
+    Operation.create(category_id: test_category_id, direction: 'income', date: '2020-01-01', amount: 100,
+      currency: 'RUB')
+    test_operation_id = Operation.find_by(amount: 100).id
+    get operation_path(test_operation_id)
     assert_response :success
   end
 
   test 'should get new' do
-    get operations_new_url
+    get new_operation_path
     assert_response :success
   end
 
   test 'should get create' do
-    get operations_create_url
-    assert_response :success
+    Category.create(name: 'Test name')
+    test_category_id = Category.find_by(name: 'Test name').id
+    post operations_path, params: {operation: { category_id: test_category_id, direction: 'income', date: '2020-01-01', amount: 100,
+      currency: 'RUB' }}
+    assert_response :found
   end
 
   test 'should get edit' do
-    get operations_edit_url
+    Category.create(name: 'Test name')
+    test_category_id = Category.find_by(name: 'Test name').id
+    Operation.create(category_id: test_category_id, direction: 'income', date: '2020-01-01', amount: 100,
+      currency: 'RUB')
+    test_operation_id = Operation.find_by(amount: 100).id
+    get edit_operation_path(test_operation_id)
     assert_response :success
   end
 
   test 'should get update' do
-    get operations_update_url
-    assert_response :success
+    Category.create(name: 'Test name')
+    test_category_id = Category.find_by(name: 'Test name').id
+    Operation.create(category_id: test_category_id, direction: 'income', date: '2020-01-01', amount: 100,
+      currency: 'RUB')
+    test_operation_id = Operation.find_by(amount: 100).id
+    patch operation_path(test_operation_id), params: { operation: { category_id: test_category_id, direction: 'income', date: '2020-01-01', amount: 200,
+      currency: 'USD' } }
+    assert_response :found
+  end
+
+  test 'should get destroy' do
+    Category.create(name: 'Test name')
+    test_category_id = Category.find_by(name: 'Test name').id
+    Operation.create(category_id: test_category_id, direction: 'income', date: '2020-01-01', amount: 100,
+      currency: 'RUB')
+    test_operation_id = Operation.find_by(amount: 100).id
+    delete operation_path(test_operation_id)
+    assert_response :see_other
   end
 end
