d2411dee5b0b5acb9b32c7d72e133df7f2740e5c
vladislav <repin_vladislav_spb@mail.ru>
2022-08-04 23:54:50 +0300

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-    validates :name, presence: true
-    validates :email, presence: true, uniqueness: true,
-      length: { minimum: 4, maximum: 254 }
-      
-    validates :password, presence: true
-  end
-  
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true,
+                    length: { minimum: 4, maximum: 254 }
+
+  validates :password, presence: true
+end
