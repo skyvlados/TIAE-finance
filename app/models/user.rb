10e0024681c1b2b02fb8b7fa9d80a20127257497
vladislav <repin_vladislav_spb@mail.ru>
2022-08-08 18:22:58 +0300

    add default scope to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  default_scope { where(is_deleted: false) }
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true,
                     length: { minimum: 4, maximum: 254 }
