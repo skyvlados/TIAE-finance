f8dda19411f7e69956090261cbb8beeb107652dd
vladislav <repin_vladislav_spb@mail.ru>
2022-08-11 22:30:27 +0300

    fix migrate

--- db/migrate/20220804080913_create_users.rb
+++ db/migrate/20220804080913_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :name, null: false
       t.string :email, null: false, unique: true
-      t.string :password_digest, null: false
+      t.string :password, null: false
       t.boolean :is_deleted, default: false
 
       t.timestamps
