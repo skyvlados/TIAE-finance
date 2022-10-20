93bc8e5714cd415696231132c6526f9b8b38d25d
vladislav <repin_vladislav_spb@mail.ru>
2022-10-20 15:46:12 +0300

    refactor

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,10 @@ Rails.application.routes.draw do
   post 'send_confirm_email' => 'sessions#send_confirm_email', as: 'send_confirm_email'
   delete 'logout' => 'sessions#destroy'
   scope '/password' do
-    get '/forgot_password', to: 'password_recovery#forgot_password'
-    post 'password_recovery', to: 'password_recovery#password_recovery'
-    get '/confirm_recovery_password/:token', to: 'password_recovery#set_new_password_form', as: 'set_new_password_form'
-    post 'set_new_password', to: 'password_recovery#set_new_password'
+    get '/forgot', to: 'password_recovery#forgot_password', as: 'forgot_password'
+    post 'recovery', to: 'password_recovery#password_recovery', as: 'password_recovery'
+    get '/confirm_recovery/:token', to: 'password_recovery#set_new_password_form', as: 'set_new_password_form'
+    post 'new', to: 'password_recovery#set_new_password', as: 'set_new_password'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
