d01c3aaacdb4f12b05c9c23fe5ab97ad2ae70026
vladislav <repin_vladislav_spb@mail.ru>
2022-10-20 15:36:57 +0300

    fix by discussions

--- app/helpers/button_helper.rb
+++ app/helpers/button_helper.rb
@@ -5,8 +5,6 @@ module ButtonHelper
     case controller.action_name
     when 'new' then 'Save'
     when 'edit' then 'Edit'
-    when 'forgot_password' then 'Sent instruction to email'
-    when 'password_recovery' then 'Set new password'
     else 'Submit'
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,12 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   post 'send_confirm_email' => 'sessions#send_confirm_email', as: 'send_confirm_email'
   delete 'logout' => 'sessions#destroy'
-  get '/forgot_password', to: 'password_recovery#forgot_password', as: 'forgot_password'
-  post 'password_recovery', to: 'password_recovery#password_recovery', as: 'password_recovery'
-  get '/confirm_recovery_password/:token', to: 'password_recovery#set_new_password_form', as: 'set_new_password_form'
-  post 'set_new_password', to: 'password_recovery#set_new_password', as: 'set_new_password'
+  scope '/password' do
+    get '/forgot_password', to: 'password_recovery#forgot_password'
+    post 'password_recovery', to: 'password_recovery#password_recovery'
+    get '/confirm_recovery_password/:token', to: 'password_recovery#set_new_password_form', as: 'set_new_password_form'
+    post 'set_new_password', to: 'password_recovery#set_new_password'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
