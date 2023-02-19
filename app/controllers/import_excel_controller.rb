683b1c01e79b5f96feea20e69e4e507d58e2fcb4
vladislav <repin_vladislav_spb@mail.ru>
2023-02-19 19:57:11 +0300

    revert commit from another pull request

--- app/controllers/import_excel_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class ImportExcelController < ApplicationController
-    def create
-        CreateOperationsFromExcel.new(current_user, '/Users/vladislav/My_documents/operation2.xls').call
-        redirect_to operations_path
-    end
-  end
-  
