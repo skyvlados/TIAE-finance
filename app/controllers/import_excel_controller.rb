87f1931b7eca8a1e780a3ce68ad745a3733e72c1
skyvlados <93619665+skyvlados@users.noreply.github.com>
2023-04-01 19:15:43 +0300

    Import excel file (#36)
    * import excel file throw path file
    
    * add select excel file throw form
    
    * add check empty file or another format file and add gem pry
    
    * bundle install
    
    * fix by discussion
    
    * return ensure and add transaction by discussion
    
    * fix by rubocop
    
    * fix by discussions
    
    * fix discussions

---
+++ app/controllers/import_excel_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class ImportExcelController < ApplicationController
+  def new; end
+
+  def create
+    if file.present? && file.original_filename.include?('.xls')
+
+      WithFile.new(file).call do |file|
+        CreateOperationsFromExcel.new(current_user, file.path).call
+      end
+
+      flash[:notice] = 'Operations successfully saved!'
+      redirect_to operations_path
+    else
+      flash[:notice] = 'You have not selected a file or selected another format file. Try again!'
+      redirect_to new_import_path
+    end
+  end
+
+  def file
+    params[:file]
+  end
+end
