ba3bd978585fc2b2e8c077fa47b5f54293ddd029
skyvlados <93619665+skyvlados@users.noreply.github.com>
2023-05-27 11:24:31 +0300

    Add docker (#38)
    * add docker
    
    * delete production from database.yml
    
    * rename db

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "tableIncomAndExpeditureWithERB_production"
+  # config.active_job.queue_name_prefix = "tiae_production"
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
