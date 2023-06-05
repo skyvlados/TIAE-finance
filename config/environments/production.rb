9726348cd322787641ce90488c84a54b755c31fa
vladislav <repin_vladislav_spb@mail.ru>
2023-06-05 07:52:15 +0300

    change http adress for registration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'https://tiaefinance.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://tiae.yakushka.ru/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.mail.ru',
