ddfd3f0569559c82ff542524d7778ee0d5e21d0d
Eugene Yak <geneayak@gmail.com>
2025-02-14 10:37:16 +0300

    domain retention (#51)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
-  config.hosts << 'tiae.yakushka.ru'
+  config.hosts << 'tiae.ykvchk.ru'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -67,7 +67,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'https://tiae.yakushka.ru/' }
+  config.action_mailer.default_url_options = { host: 'https://tiae.ykvchk.ru/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.mail.ru',
