73f36adeae041a1a74830234721e9cbd828f22b8
vladislav.rep <repin_vladislav_spb@mail.ru>
2026-03-09 23:44:31 +0300

    Correction of html forms. Add page size for web of mobile version method. Add find by name for categories.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   include Pagy::Backend
 
+  private
+
   def current_user
     if Rails.env.development?
       telegram_id = 1
@@ -15,6 +17,18 @@ class ApplicationController < ActionController::Base
 
     @current_user ||= User.find_or_create_by(telegram_id: telegram_id) do |user|
       user.name = name
+    end
+  end
+
+  def mobile_device?
+    request.user_agent =~ /Mobile|webOS/
+  end
+
+  def page_size(page_size)
+    if page_size.present?
+      page_size.to_i
+    else
+      mobile_device? ? 5 : 10
     end
   end
 end
