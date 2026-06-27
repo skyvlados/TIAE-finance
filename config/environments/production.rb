49b61a09f3f8607ee32626bb422ee1f1897c76bc
skyvlados <93619665+skyvlados@users.noreply.github.com>
2026-06-27 22:16:26 +0300

    Change domain TLD. (#62)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
-  config.hosts << 'tiae.repka-rvn.ru'
+  config.hosts << 'tiae.repka-rvn.pro'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
